d8035eafdffb26bb0f0548b05faf6f7b66f34205
sophiedebrabander <sophie.debrabander@wijs.be>
2014-04-30 22:33:07 +0200

    Added .gitignore

---
+++ layout/js/memory.js
@@ -0,0 +1,38 @@
+var Memory =
+{
+	init: function()
+	{
+		Memory.addTwoPlayers();
+		Memory.createRandomCards();
+		Memory.shuffleCards();
+		Memory.isMatch();
+		Memory.flipoverAnimation();
+	},
+
+	addTwoPlayers: function()
+	{
+
+	},
+
+	createRandomCards: function()
+	{
+
+	},
+
+	shuffleCards: function()
+	{
+
+	},
+
+	isMatch: function()
+	{
+
+	},
+
+	flipoverAnimation: function()
+	{
+
+	}
+}
+
+Memory.init();
