9d4450bdfbf9ce00c286db18fa01efca025a53f8
sophiedebrabander <sophie.debrabander@wijs.be>
2014-05-14 20:14:24 +0200

    Start card to objects

--- layout/js/memory.js
+++ layout/js/memory.js
@@ -1,45 +1,75 @@
 Memory = {
 
-  settings: {
-	maxPlayers: 4
-  },
+	settings: {
+		maxPlayers: 4,
+		cardsAmount: 24
+	},
 
-  elements: {
-	addPlayer: $( ".js-add-player" ),
-	playerName: $('.player-name'),
-	scoreBoard: "<div>scoreboard shizzle comes here</div>",
-	playNow: $('.play-now')
-  },
+	elements: {
+		addPlayer: $('.js-add-player'),
+		board: $('#board'),
+		playerName: $('.player-name'),
+		scoreBoard: '<div>scoreboard shizzle comes here</div>',
+		playNow: $('.play-now'),
+		card: '<div class=\'grid-2\'></div>'
+	},
 
-  init: function() {
-	settings = this.settings;
-	elements = this.elements;
-	this.AddPlayers();
-  },
+	// @todo push players to object
+	players: new Object(),
 
-  AddPlayers: function() {
-	elements.addPlayer.submit(function(event) {
-		event.preventDefault();
-		$("ul").append("<li>" + elements.playerName.val() + elements.scoreBoard+ "</li>");
+	cards: new Object(),
 
-		var listLength =  $( ".players-list li").length;
-		switch(true) {
-			case listLength === 2:
-			$("input").removeClass('is-hidden');
-			break;
+	init: function() {
+		settings = this.settings;
+		elements = this.elements;
+		this.AddPlayers();
+		this.createBoard();
+	},
 
-			case listLength === settings.maxPlayers:
-			elements.addPlayer.hide();
-			break;
+	AddPlayers: function() {
+		elements.addPlayer.submit(function(event) {
+			event.preventDefault();
+			$('ul').append('<li>' + elements.playerName.val() + elements.scoreBoard+ '</li>');
+
+			var listLength =  $( '.players-list li').length;
+			switch(true) {
+				case listLength === 2:
+				$('input').removeClass('is-hidden');
+				break;
+
+				case listLength === settings.maxPlayers:
+				elements.addPlayer.hide();
+				break;
+			}
+		});
+	},
+
+	createBoard: function() {
+		var numVariations = settings.cardsAmount / 2;
+
+		for ( var i = 1; i <= numVariations; i++){
+			var cardOne = $(elements.card);
+			cardOne.html(i);
+
+			var cardTwo = $(elements.card);
+			cardTwo.html(i);
+
+
+			elements.board.append(cardOne);
+			elements.board.append(cardTwo);
+
+			Memory.cards[i] = cardOne;
+			Memory.cards[i] = cardTwo;
 		}
-	});
-},
 
+	}
+};
+
+// @todo form validation
 // @todo create cards
 // @todo hide content cards
 // @todo shuffle cards
 // @todo is match when 2 clicked
 // @todo flipoverAnimation
-};
 
 Memory.init();
